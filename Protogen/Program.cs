86efde33dbf214c10b68b8ce6e449b37e00e5eef
Jonathan Johnson <jon@nilobject.com>
2017-09-10 08:50:04 -0700

    Add project files.

---
+++ Protogen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Protogen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
